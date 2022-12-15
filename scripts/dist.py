d2ecce8ff233837e308daa5e17820030ed288e7a
Benjamin Gilbert <bgilbert@backtick.net>
2022-12-15 12:42:36 -0500

    meson: include docs in dist tarball

--- scripts/dist.py
+++ scripts/dist.py
@@ -10,3 +10,7 @@ base = Path(os.getenv('MESON_DIST_ROOT'))
 # remove Git metadata from tarball
 base.joinpath('.gitignore').unlink()
 shutil.rmtree(base / '.github')
+
+# build docs and add them to the tarball
+subprocess.run(['meson', 'compile', 'html'], check=True)
+shutil.copytree('html', base / 'doc/html', symlinks=True)
