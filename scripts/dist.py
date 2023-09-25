eade33fbfc1388c5322cac99c00cf6294a846524
Benjamin Gilbert <bgilbert@backtick.net>
2023-09-25 09:46:58 -0500

    meson: omit Sphinx doctrees from dist tarball
    They're only needed when building the documentation.  Saves 1.9 MiB
    uncompressed, 0.2 MiB compressed.

--- scripts/dist.py
+++ scripts/dist.py
@@ -14,3 +14,4 @@ shutil.rmtree(base / '.github')
 # build docs and add them to the tarball
 subprocess.run(['meson', 'compile', 'html'], check=True)
 shutil.copytree('html', base / 'doc/html', symlinks=True)
+shutil.rmtree(base / 'doc/html/.doctrees')
